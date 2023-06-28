c3a6568e8c3bd3d9c6399931e9181b8716f8a823
Sina Shirparvar <s.shirparvar@afranet.ir>
2023-06-28 22:13:51 +0330

    adding restful package and define user resource

---
+++ authz/controller/apiv1/user.py
@@ -0,0 +1,19 @@
+from authz.util import jsonify
+
+
+class UserController:
+
+    def create_user():
+        return jsonify(status=501)
+
+    def get_user_list():
+        return jsonify(status=501)
+
+    def get_user(user_id):
+        return jsonify(status=501)
+
+    def update_user(user_id):
+        return jsonify(status=501)
+
+    def delete_user(user_id):
+        return jsonify(status=501)
---
+++ authz/resource/apiv1/user.py
@@ -0,0 +1,36 @@
+from flask_restful import Resource
+
+from authz.controller.apiv1 import UserController
+
+
+class UserResource(Resource):
+    def get(self, user_id=None):
+        """
+        GET /users --> Get list of users.
+        Get /users/<user_id> --> Get user info.
+        """
+        if user_id is None:
+            return UserController.get_user_list()  # Get list of users
+        else:
+            return UserController.get_user()  # Get user info
+
+    def post(self):
+        """
+        POST /users --> Create new user.
+        POST /users/<user_id> --> Not allowed.
+        """
+        return UserController.create_user()  # Create new user.
+
+    def patch(self, user_id):
+        """
+        PATCH /users --> Not allowed.
+        PATCH /users/user_id --> Update user.
+        """
+        return UserController.update_user(user_id)  # Update user
+
+    def delete(self, user_id):
+        """
+        DELETE /users --> Not allowed.
+        DELETE /users/user_id --> Delete user.
+        """
+        return UserController.delete_user(user_id)  # delete user
