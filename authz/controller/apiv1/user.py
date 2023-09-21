3be2f3cde0842c117c222879642f56b1f2f32c2d
Sina Shirparvar <s.shirparvar@afranet.ir>
2023-09-21 12:56:14 +0330

    create initial database migrate

--- authz/controller/apiv1/user.py
+++ authz/controller/apiv1/user.py
@@ -1,16 +1,19 @@
 from authz.util import jsonify
 
+from authz.model import User
+from authz.schema.apiv1 import UserSchema
+
 
 class UserController:
-
-    def create_user():
-        return jsonify(status=501)
 
     def get_user_list():
         return jsonify(status=501)
 
     def get_user(user_id):
         return jsonify(status=501)
+
+    def create_user():
+        return jsonify(status=501)    
 
     def update_user(user_id):
         return jsonify(status=501)
