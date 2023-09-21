b7f5c6edf7cbd648631fb7fbe2b0de5a4dda3f60
Sina Shirparvar <s.shirparvar@afranet.ir>
2023-09-21 12:46:21 +0330

    mysql support and initialize flask migrate

--- authz/authz.py
+++ authz/authz.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from authz.config import Config
 
 db = SQLAlchemy()
+mg = Migrate()
 ma = Marshmallow()
 
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix="/api/v1")
@@ -16,6 +18,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Load from environment variables.
     db.init_app(app)  # Initialize SQLALCHEMY database object.
+    mg.init_app(app, db) # Initialize database management and migrate object.
     ma.init_app(app) #Initialize Marshmallow object.
     app.register_blueprint(apiv1_bp)
     return app
