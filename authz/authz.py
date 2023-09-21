17a877d139f66545722cc3dc2514eb2e7921868c
Sina Shirparvar <s.shirparvar@afranet.ir>
2023-09-21 11:39:25 +0330

    add and initialize flask marshmallow package

--- authz/authz.py
+++ authz/authz.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from authz.config import Config
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -14,5 +16,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Load from environment variables.
     db.init_app(app)  # Initialize SQLALCHEMY database object.
+    ma.init_app(app) #Initialize Marshmallow object.
     app.register_blueprint(apiv1_bp)
     return app
